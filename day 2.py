1f1d073183e8c000b2baacd5bcdb9d18cc949be3
Mahad Qamar Lodhi <mqamarlodhi@gmail.com>
2025-11-11 19:29:07 +0500

    Create day 2.py

---
+++ day 2.py
@@ -0,0 +1,71 @@
+#  we learn day 2 Variables, Data Types, aur Input/Output
+ # what is variable ? one name store data
+name = "Mahad" #string
+age = 22  #integer
+city = "multan" #string
+print(name)
+print(age)
+print(city)
+
+#name # age #city this is variable store data but python variable Case-sensitive (Name ≠ name)
+
+step 2 
+Data Type
+int  10 number is int
+float 10.0 number in decimal form is float
+string "mahad" name or alphabat is string
+boolean yes or no is boolean
+
+example
+a=10
+b=10.5
+c="Python"
+d=True
+print=(type(a))
+print=(type(c))
+
+step 3
+
+taking input form user
+name = input("enter your name")
+print=(name)
+Note: Input by default string hota hai.
+Step 4: Mini Practice Tasks
+
+Ask user name, age, city → print formatted output
+
+Ask two numbers → print their sum
+
+Ask product name + price → print message like
+"The perfume costs Rs. 850"
+
+
+solution
+name= input("enter your name")
+age = int(input("enter your age"))
+city = input("enter your city name")
+print (name)
+print (age)
+print (city)
+
+Two number sum
+
+a = 20
+b = 40
+print(a+b)
+
+Ask product name + price → print message like
+"The perfume costs Rs. 850"
+
+product = input("enter your product name")
+cost = int(input("enter price))
+print ("your perfume",product,"RS.",cost)                  
+                 
+
+
+
+
+
+
+
+            
