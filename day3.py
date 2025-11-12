57b80a8838d5b56c19a6e35133e8aae7dc8b8509
Mahad Qamar Lodhi <mqamarlodhi@gmail.com>
2025-11-12 18:41:08 +0500

    Create day3.py

---
+++ day3.py
@@ -0,0 +1,29 @@
+# today we read Function,Type converstion 
+#FUNCTION
+#What is Function? function is reuseable block of code jo spacific task run krta hai.
+
+#function bana k aur use krne k method
+def mahad():
+   print("Hello world!Welcome to my world")
+
+mahad() #function call
+
+#functin with parameter
+def person(name)
+   print(f"my name {name} welcome to my world")
+person(mahad)
+
+#step 3: Function with Multiple Parameters
+def person(name,age,city):
+  print(f"my name is {name},my age is {age},my city is{city})
+
+person("mahad",22,"Multan")
+ #Step 4: Function with Return Value
+
+def addnumber(a,b)
+   return=a+b
+ result = addnumber(5,10)
+ print("sum is :"result)
+
+
+  
