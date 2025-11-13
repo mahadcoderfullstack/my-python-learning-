710d07987e7b452ea7b357804ab0c799f0a429e9
Mahad Qamar Lodhi <mqamarlodhi@gmail.com>
2025-11-13 21:16:24 +0500

    Create Mini Project.py

---
+++ Mini Project.py
@@ -0,0 +1,9 @@
+#Mini Project – Simple Calculator (with Functions)
+def sum(a,b):return a+b
+def subscrtact(a,b):return a-b
+def mul(a,b):return a*b
+def div(a,b):return a/b
+
+print ("slect this option if you want this function +,-,*,/")
+op = input("enter your function")
+number1 = int(("enter"))
