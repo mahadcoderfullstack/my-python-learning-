a601b0b7d1ea4b41e12d3f61c57e37e203b70540
Mahad Qamar Lodhi <mqamarlodhi@gmail.com>
2025-11-13 19:37:04 +0500

    Create task 2.py

---
+++ task 2.py
@@ -0,0 +1,7 @@
+# Function to convert Celsius → Fahrenheit
+def c_f(celsius):
+  return (celsius * 9/5)+32
+c= float(input("enter your value))
+result = c_f(c)
+print(your fahrenheit result",result)
+  
