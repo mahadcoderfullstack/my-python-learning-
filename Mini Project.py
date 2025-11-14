9bca94edea04eb7f43c530a35093232b61e4d8bd
Mahad Qamar Lodhi <mqamarlodhi@gmail.com>
2025-11-14 17:15:56 +0500

    Update Mini Project.py

--- Mini Project.py
+++ Mini Project.py
@@ -6,4 +6,15 @@ def div(a,b):return a/b
 
 print ("slect this option if you want this function +,-,*,/")
 op = input("enter your function")
-number1 = int(("enter"))
+number1 = int(input("enter a first number"))
+number2 = int(input("enter a second number"))
+if op=="+"
+ print("you result is",a+b)
+elif op=="-"
+ print("your result is",a-b)
+elif op == "*"
+ print("your result is",a*b)
+elif op=="/"
+ print("your result is", a/b)
+else
+ print("invaild key")
