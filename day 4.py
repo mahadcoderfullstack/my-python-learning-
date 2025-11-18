9219c32e76c77706fc07fd816296eba059375b83
Mahad Qamar Lodhi <mqamarlodhi@gmail.com>
2025-11-18 19:01:06 +0500

    Create day 4.py

---
+++ day 4.py
@@ -0,0 +1,40 @@
+# today we learn string
+#what is string ?  answer string is text
+
+name ="mahad"
+print = (name)
+
+#user input string
+name = input("enter your user name")
+print =(name)
+
+#how find string len
+name = "mahad"
+print(len(name))
+#upper case use string
+print(name.upper())
+#lower case use string
+print(name.lowercase())
+#space remove strip
+print(name.strip())
+#replace case change character
+print (name.replace("mahad","saim")
+  # find search in code
+  print(name.find("mahad"))
+#most important part string scling
+
+name = "mahadqamar"
+print(name[0]) #output m
+print(name[0:5]) #utput mahad
+print(name[:5]) #output mahad
+print(name[5:]) #output qamar
+#String Concatenation
+
+
+#task User se name, age, city input lo aur ek line me print karo:
+name= input("enter your name")
+age = int(input("enter your age")
+city = input("enter your city")
+print(f"your name {name},your age {age},your city {city})
+
+       
